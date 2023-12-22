a1a519d9d4c2833888b70276c6f19e3e29181b9b
Xavier MALPARTY <xavier@nimblehq.co>
2023-12-22 11:19:49 +0700

    First tentative with basic file copy to test on published NPX package

--- packages/next-js-cli/bin.js
+++ packages/next-js-cli/bin.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
+var fs = require('fs');
 
-const yargs = require('yargs')
+const yargs = require('yargs');
+const brandName = yargs.argv['brand']
 
-console.log(yargs.argv)
+if (yargs.argv['help'] || yargs.argv['h']) {
+  return console.log('Args: "--brand", set the name of the brand to scaffold the project for.');
+}
+if (brandName === undefined || brandName == '') {
+  return console.log('The "--brand" argument is required.');
+}
+
+// Copy the template
+fs.cpSync('../next-js-template/template', `./${brandName}`, { recursive: true });
+
+// Rename the project name in the package.json
+// WIP
+
+console.log(`Project scaffolded. Now "cd ./${brandName}" and run "npm install"`);
