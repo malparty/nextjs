4c99198d9d0b6418e0ab8ddea6faeec98064ae8a
Xavier MALPARTY <xavier@nimblehq.co>
2023-12-22 11:55:33 +0700

    Test using npm pack cli

--- packages/next-js-cli/bin.js
+++ packages/next-js-cli/bin.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
-var fs = require('fs');
 
+const decompress = require('decompress');
+const decompressTargz = require('decompress-targz');
+const fs = require('fs');
+const { exec } = require('child_process');
 const yargs = require('yargs');
-const brandName = yargs.argv['brand']
 
+const template_package = '@malparty/nextjs-template';
+const brandName = yargs.argv['brand'];
+
+// Arguments validation
 if (yargs.argv['help'] || yargs.argv['h']) {
   return console.log('Args: "--brand", set the name of the brand to scaffold the project for.');
 }
@@ -11,10 +17,27 @@ if (brandName === undefined || brandName == '') {
   return console.log('The "--brand" argument is required.');
 }
 
-// Copy the template
-fs.cpSync('../next-js-template/template', `./${brandName}`, { recursive: true });
+// Download the template package using the NPM cli (to allow authenticated requests)
+exec(`npm pack ${template_package}`, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  decompressTemplate(stdout);
+});
 
-// Rename the project name in the package.json
-// WIP
+// Decompress the tgz template
+const decompressTemplate = (packageName) => {
+  decompress(packageName, brandName, {
+    plugins: [decompressTargz()],
+  }).then(() => {
+    console.log('Files decompressed');
 
-console.log(`Project scaffolded. Now "cd ./${brandName}" and run "npm install"`);
+    console.log(`Project scaffolded. Now "cd ./${brandName}" and run "npm install"`);
+  });
+};
